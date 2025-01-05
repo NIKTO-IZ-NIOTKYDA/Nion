4645efe88a87961c9611cbf63b1457fef16da6e3
NiK <a.s7eve777@gmail.com>
2025-01-06 00:56:42 +0500

    Fix Fix TelegramForbiddenError

--- bot/handlers/menu.py
+++ bot/handlers/menu.py
@@ -1,7 +1,7 @@
 from aiogram import F
 from aiogram.types import CallbackQuery
 from aiogram.fsm.context import FSMContext
-from aiogram.exceptions import TelegramForbiddenError
+from aiogram.exceptions import TelegramBadRequest
 
 from other.config import config
 from utils import CheckAuthUser
@@ -32,5 +32,5 @@ async def menu(callback: CallbackQuery, state: FSMContext):
                 [role['role_id'] for role in user['roles']]
             )
 
-        except TelegramForbiddenError:
+        except TelegramBadRequest:
             await start(callback.message)
