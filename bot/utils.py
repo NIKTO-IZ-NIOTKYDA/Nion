c0194d9795e8fcc3b8245b7d6bd80f65c7126ed8
NIK <141701604+NIKTO-IZ-NIOTKYDA@users.noreply.github.com>
2025-01-16 16:57:59 +0500

    FIX
    Signed-off-by: NIK <141701604+NIKTO-IZ-NIOTKYDA@users.noreply.github.com>

--- bot/utils.py
+++ bot/utils.py
@@ -63,15 +63,6 @@ async def newsletter(user_id: int, text: str, auto: bool, bot: aiogram.Bot) -> N
 
         except (TelegramForbiddenError, TelegramBadRequest):
             log.warn(str(user['user_id']), f'User {user['user_id']} has blocked the bot!')
-            await rq_users.SetUser(
-                user['user_id'],
-                user['username'],
-                user['first_name'],
-                user['last_name'],
-                user['send_notifications'],
-                True,
-                user['role_ids']
-            )
 
             user = await rq_users.GetUser(user['user_id'])
             await rq_users.SetUser(
